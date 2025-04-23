19cf26e0d45902bdd5cb3e05fc7215593714ea6f
sue <sleepy75kr@gmail.com>
2025-04-23 21:47:26 +1200

    normalizeURLs

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
+# Setup
+
 1. clone repository:
-git clone https://github.com/hjkr/http-project.git
+`git clone https://github.com/hjkr/http-project.git`
 2. install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    `curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh`
 3. install node:
-nvm install
+    `nvm install`
 4. excute `nvm use`
 5. execute `npm init`
-6. add "start":"node main.js" into package.json
-![alt text](image.png)
+6. add `"start":"node main.js"` into package.json
+
+![alt text](/assets/image.png)
+7. install Jest:
+`npm install --save-dev jest`
+8. create .gitigmore and add node_modules folders:
+9. modify package.json -> add jest
+![alt text](/assets/image-1.png)
+
 
---
+++ tests/crawl.test.js
@@ -0,0 +1,30 @@
+const {normalizeURL} = require('../crawl.js');
+const {test, expect} = require('@jest/globals');
+
+test('normalizeURL strip', () => {
+    const input = 'https://blog.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+})
+
+test('normalizeURL with /', () => {
+    const input = 'https://blog.boot.dev/path/';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+})
+
+test('normalizeURL capitals', () => {
+    const input = 'https://BLOG.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+})
+
+test('normalizeURL capitals', () => {
+    const input = 'http://blog.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+})
